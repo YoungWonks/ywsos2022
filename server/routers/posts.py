02ee912b5c8bf3d1fe80d3e14ccd5fdb008abc95
Aditya Maram <aditya.maram@gmail.com>
2022-09-15 17:08:02 -0700

    Fixing the routes and returning posts

--- server/routers/posts.py
+++ server/routers/posts.py
@@ -5,21 +5,28 @@ from fastapi.responses import JSONResponse
 from config import settings
 from db import db
 from models.post import PostModel
+from typing import List
 
 router = APIRouter()
 
-@router.get('/')
-def get_posts():
-    return {"message": f"This returns all posts"}
-@router.get('/{id}')
-def get_post():
-    return {"message": "This post based on ID", "success": True}
+@router.get('/',response_model=List[PostModel])
+async def get_posts():
+    all_posts = await db.posts.find({}).to_list(1000)
+    print(type(all_posts))
+    
+    return JSONResponse(status_code=status.HTTP_200_OK,content=all_posts)
+@router.get('/{id}',response_model=PostModel)
+async def get_post(id:str):
+    post = await db.posts.find_one({"_id":id})
+    if post is None:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,content="Post not Found")
+    return JSONResponse(status_code=status.HTTP_200_OK,content=post) 
 @router.post('/',response_model=PostModel)
 async def add_post(post:PostModel=Body(...)):
     post = jsonable_encoder(post)    
     new_post = await db.posts.insert_one(post)
     created_post = await db.posts.find_one({"_id":new_post.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED,data=created_post)
+    return JSONResponse(status_code=status.HTTP_201_CREATED,content=created_post)
 @router.put('/{id}')
 def update_post():
     return {"message": "this updates a post", "success": True}
