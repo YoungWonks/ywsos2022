e4f65d81cd33d1245ea947a8d759185fda333111
Saketh Parimi <sp2007firebase@gmail.com>
2022-11-05 17:38:15 -0700

    I made the theming more concise.

--- mobile/lib/src/app.dart
+++ mobile/lib/src/app.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:mobile/src/core/bindings/initial_bindings.dart';
+import 'package:mobile/src/core/settings/themes/theme_service.dart';
 import 'package:mobile/src/core/settings/themes/themes.dart';
 import 'package:mobile/src/features/auth_feature/login_view.dart';
-import 'package:mobile/src/core/settings/themes/themes_controller.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
@@ -14,11 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themeController = Get.put(ThemeController());
-
     return GetMaterialApp(
       initialBinding: InitialBinding(),
-      themeMode: themeController.theme,
+      themeMode: ThemeService().getThemeMode(),
       theme: Themes.lightTheme,
       darkTheme: Themes.darkTheme,
       debugShowCheckedModeBanner: false,
