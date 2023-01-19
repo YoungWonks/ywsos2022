e4ce1279dfc6b2a28d9c3b7db4085d6beb8fa4c4
Merisa Halperin <spshalperin@gmail.com>
2023-01-18 21:10:28 -0500

    Working on frontend username validating

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="container">
+  <div class="d-flex align-self-center flex-column bg-white bg-opacity-50 p-5 rounded-3">
     <form>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
+        <label for="exampleInputEmail1" class="form-label">Email address/username</label>
+        <input type="text" class="form-control" :class="[validateUsername?'is-valid':'is-invalid']" id="exampleInputEmail1" aria-describedby="emailHelp" v-model = "username"/>
+        <div class = "invalid-feedback" v-show = "!validateUsername">
+          <p>
+            Username is invalid. Usernames must be 3 characters long.
+          </p>
+        </div>
         <div id="emailHelp" class="form-text">
           We'll never share your email with anyone else.
         </div>
@@ -23,13 +23,30 @@
       </div>
       <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <label class="form-check-label" for="exampleCheck1">Remember me</label>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-secondary m-3">Submit</button>
+      <a href = "/login">
+        <button class="btn btn-secondary">Login instead</button>
+      </a>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data(){
+    return{username:""}
+  },
+  computed:{
+    validateUsername(){
+      if (this.username.length < 3){
+        return false
+      }
+      else{
+        return true
+      }
+    }
+  }
+};
 </script>
