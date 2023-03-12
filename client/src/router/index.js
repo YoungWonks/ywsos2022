4a2c71602c8eabe1f84d691ad7f7c5b1cdb35a32
kohli <86390549+notskiing@users.noreply.github.com>
2023-03-12 09:38:57 -0700

    forgot password frontend complete

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -24,6 +24,11 @@ const router = createRouter({
       name: "settings",
       component: () => import("../views/SettingsView.vue"),
     },
+    {
+      path: "/forgotpass",
+      name: "forgotPass",
+      component: () => import("../views/ForgotPassView.vue"),
+    },
   ],
 });
 
---
+++ client/src/views/ForgotPassView.vue
@@ -0,0 +1,12 @@
+<template>
+    <ForgotPass/>
+
+</template>
+<script>
+    import ForgotPass from "../components/ForgotPass.vue" 
+    export default {
+    components: {
+        ForgotPass,
+    },
+    };
+</script>
